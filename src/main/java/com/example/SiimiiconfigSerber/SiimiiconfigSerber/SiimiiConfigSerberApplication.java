b59ba37f35df0f5a3acf4b7187cd8480662225d7
Your Name <altair2400@gmail.com>
2021-08-10 10:55:47 +0200

    maybe fixed

--- src/main/java/com/example/SiimiiconfigSerber/SiimiiconfigSerber/SiimiiConfigSerberApplication.java
+++ src/main/java/com/example/SiimiiconfigSerber/SiimiiconfigSerber/SiimiiConfigSerberApplication.java
@@ -2,10 +2,12 @@ package com.example.SiimiiconfigSerber.SiimiiconfigSerber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class SiimiiConfigSerberApplication {
 
 	public static void main(String[] args) {
@@ -13,3 +15,4 @@ public class SiimiiConfigSerberApplication {
 	}
 
 }
+
