a58f6daaeca9b9d9734e70d0817e503e307ac7a5
Your Name <altair2400@gmail.com>
2021-08-10 08:50:45 +0200

    first commit

---
+++ src/test/java/com/example/SiimiiconfigSerber/SiimiiconfigSerber/SiimiiConfigSerberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SiimiiconfigSerber.SiimiiconfigSerber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiimiiConfigSerberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
